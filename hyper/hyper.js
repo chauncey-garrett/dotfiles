059bb34a9eee9dbe72d475e1df77d47839395463
Chauncey Garrett <git@chauncey.io>
2017-03-30 16:41:12 -0500

    Update Hyper config

--- hyper/hyper.js
+++ hyper/hyper.js
@@ -1,16 +1,23 @@
+// Future versions of Hyper may add additional config options,
+// which will not automatically be merged into this file.
+// See https://hyper.is#cfg for all currently supported options.
 module.exports = {
   config: {
+
     // default font size in pixels for all tabs
     fontSize: 15,
 
     // font family with optional fallbacks
-    fontFamily: '"Essential Pragmata Pro", "Input Mono Narrow Regular", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    fontFamily: '"FuraCode Nerd Font", "Essential Pragmata Pro", "Input Mono Narrow", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
     cursorColor: 'rgba(248,28,229,0.8)',
 
     // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
     cursorShape: 'BLOCK',
+
+    // set to true for blinking cursor
+    cursorBlink: true,
 
     // color of the text
     foregroundColor: '#fff',
@@ -26,6 +33,15 @@ module.exports = {
 
     // custom css to embed in the terminal window
     termCSS: '',
+
+    // set to `true` (without backticks) if you're using a Linux setup that doesn't show native menus
+    // default: `false` on Linux, `true` on Windows (ignored on macOS)
+    showHamburgerMenu: '',
+
+    // set to `false` if you want to hide the minimize, maximize and close buttons
+    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
+    // default: `true` on windows and Linux (ignored on macOS)
+    showWindowControls: '',
 
     // custom padding (css format, i.e.: `top right bottom left`)
     padding: '12px 14px',
@@ -54,6 +70,9 @@ module.exports = {
 
     // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
     // if left empty, your system's login shell will be used by default
+    // make sure to use a full path if the binary name doesn't work
+    // (e.g `C:\\Windows\\System32\\bash.exe` instead of just `bash.exe`)
+    // if you're using powershell, make sure to remove the `--login` below
     shell: '',
 
     // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
@@ -69,6 +88,10 @@ module.exports = {
     // if true, selected text will automatically be copied to the clipboard
     copyOnSelect: true
 
+    // if true, on right click selected text will be copied or pasted if no
+    // selection is present (true by default on Windows)
+    // quickEdit: true
+
     // URL to custom bell
     // bellSoundURL: 'http://example.com/bell.mp3',
 
@@ -83,16 +106,18 @@ module.exports = {
   //   `project#1.0.1`
   plugins: [
     "hyper-solarized-dark",
-    "opaque-cursor",
     "hyper-font-smoothing",
-    "hyperambient",
-    "hyper-blink",
     "hypercwd",
-    "hyperterm-mactabs",
     "hyperterm-1password",
     "hyperlinks",
     "hyperterm-focus-reporting",
-    "hyperterm-dibdabs"
+    "hyperterm-dibdabs",
+    "config-hyperterm",
+    "hyperterm-tabs",
+    "hyperterm-open-devtools",
+    "hyperterm-alternatescroll",
+    "hyperterm-visor",
+    "hyperterm-paste"
   ],
 
   // in development, you can create a directory under
